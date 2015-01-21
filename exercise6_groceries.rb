5865fd74a28cbce148d9ea65eb91280a98aeba48
jhc <coombsjh@gmail.com>
2015-01-21 15:16:12 -0500

    version1 instructions

---
+++ exercise6_groceries.rb
@@ -0,0 +1,21 @@
+exercise6)_groceries.rb
+grocery_list =["beans", "milk", "chocolate"]
+puts grocery_list
+#make array
+# add rice
+grocery_list << "rice"
+# output grocery_list
+
+# make a method to output the grocery_list
+# grocery_list.each { |i| puts i }  add this to print each
+# count the grocery_list
+# ask include?"bananas"
+# if yes puts "you don't need bananas"
+# if no puts "you need to pick up bananas"
+# show the second item in list grocery_list[1]
+# alphabetize
+# print to screen
+# remove rice from list
+# print again
+
+# hash_syntax[:five] = 5
