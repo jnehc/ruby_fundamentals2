0bd5e9890867f47e195e50ea40e4f2539ef1cd5f
jhc <coombsjh@gmail.com>
2015-01-21 21:43:02 -0500

    working verion, needs better formatting

--- exercise6_groceries.rb
+++ exercise6_groceries.rb
@@ -1,33 +1,20 @@
 grocery_list =["beans", "milk", "chocolate"]
-#make array
-# add rice
-grocery_list << "rice"
-# output grocery_list
-# make a method to output the grocery_list
 def print_list(my_list)
 	my_list.each {|x| print "* ",x,"\n" }
 end 
 print_list(grocery_list)
-
-# count the grocery_list
+grocery_list << "rice"
+print_list(grocery_list)
 puts grocery_list.count
-# ask include?"bananas"
 if grocery_list.include?("bananas")
     puts "Good you already have bananas"
 	else
 	puts "Don't forget to buy bananas"
 end
-# if yes puts "you don't need bananas"
-# if no puts "you need to pick up bananas
-# show the second item in list grocery_list[1]
-puts grocery_list[1]
-# alphabetize
+puts "The second item in the list is:", grocery_list[1]
 grocery_list.sort!
-puts grocery_list
 print_list(grocery_list)
-# remove rice from list
 grocery_list.delete("rice")
-# print again
 print_list(grocery_list)
 
 
