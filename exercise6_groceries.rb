eef9269d49e970319aa0d67ece8a709a6856d56d
jhc <coombsjh@gmail.com>
2015-01-21 21:30:17 -0500

    print and count working

--- exercise6_groceries.rb
+++ exercise6_groceries.rb
@@ -1,30 +1,33 @@
 grocery_list =["beans", "milk", "chocolate"]
-puts grocery_list
 #make array
 # add rice
 grocery_list << "rice"
 # output grocery_list
 # make a method to output the grocery_list
-def print_list(i)
-	grocery_list.each { |i| puts i } 
+def print_list(my_list)
+	my_list.each {|x| print "* ",x,"\n" }
 end 
+print_list(grocery_list)
+
 # count the grocery_list
-grocery_list.count
+puts grocery_list.count
 # ask include?"bananas"
-if grocery_list.include? "bananas"
+if grocery_list.include?("bananas")
     puts "Good you already have bananas"
 	else
 	puts "Don't forget to buy bananas"
 end
 # if yes puts "you don't need bananas"
-# if no puts "you need to pick up bananas"
+# if no puts "you need to pick up bananas
 # show the second item in list grocery_list[1]
-grocery_list[1]
+puts grocery_list[1]
 # alphabetize
-grocery_sort = grocery_list.sort
+grocery_list.sort!
+puts grocery_list
 print_list(grocery_list)
 # remove rice from list
 grocery_list.delete("rice")
 # print again
 print_list(grocery_list)
+
 
